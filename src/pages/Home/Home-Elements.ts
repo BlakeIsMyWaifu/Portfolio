53888a979467569be8d65b6161b323c46ec0c237
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-03-09 17:44:10 +0000

    home page

---
+++ src/pages/Home/Home-Elements.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import { cream, silverPink } from 'utils/colours'
+
+export const TextContainer = styled.div`
+	padding: 10%;
+`
+
+export const TextHello = styled.h2`
+	color: ${cream};
+	font-size: 1rem;
+	margin-left: 2rem;
+`
+
+export const TextName = styled.h1`
+	color: ${silverPink};
+	font-size: 4rem;
+`
+
+export const TextJobTitle = styled.h2`
+	color: ${cream};
+	font-size: 2rem;
+	margin-left: 1rem;
+`
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,21 @@
 import PageContainer from 'components/PageContainer'
 import React from 'react'
 
+import { TextContainer, TextHello, TextJobTitle, TextName } from './Home-Elements'
+
 const Home: React.FC = () => {
+
+	const textHello = 'Hello, I\'m'
+	const textName = 'Joel Dohm-Karatjas'
+	const textJobTitle = 'Junior Front End Web Developer'
 
 	return (
 		<PageContainer>
-			home
+			<TextContainer >
+				<TextHello>{textHello}</TextHello>
+				<TextName>{textName}</TextName>
+				<TextJobTitle>{textJobTitle}</TextJobTitle>
+			</TextContainer>
 		</PageContainer>
 	)
 }
