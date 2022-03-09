9157080d293c82a738e388f4e8fc2e161af0a3f0
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-03-09 18:14:22 +0000

    about page

---
+++ src/components/Text/Text-Elements.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { cream, silverPink } from 'utils/colours'
+
+export const Title = styled.h1`
+	color: ${silverPink};
+	font-size: 5rem;
+	padding: 2rem;
+`
+
+export const Paragraph = styled.p`
+	color: ${cream};
+	font-size: 2rem;
+	white-space: pre-wrap;
+`
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,21 @@
 import PageContainer from 'components/PageContainer'
+import { Paragraph, Title } from 'components/Text/Text-Elements'
 import React from 'react'
 
 const About: React.FC = () => {
 
+	const paragraph = [
+		'Hi, I\'m a 21 year old self taught programmer from London.',
+		'Ever since I was little I\'ve had an interest in programming and technology. Over the last six years I\'ve managed to turn that interest into a hobby and passion. Now i\'m looking to that it that next step and turn it into a career.',
+		'Currently I\'m an entry level front end developer currently looking for employment. I\'ve been learning and using JavaScript for six years and React for two as well as various other languages and technologies. I\'m seeking a React developer position to purse my passion for web development and creating new experiences.'
+	].join('\n\n')
+
 	return (
 		<PageContainer>
-			about
+			<Title>About Me</Title>
+			<Paragraph>
+				{paragraph}
+			</Paragraph>
 		</PageContainer>
 	)
 }
