c79d67745687c76567209696562a5ff8a3478b73
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-04-01 16:14:15 -0700

    updated React version

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOMClient from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+const container = document.getElementById('root') as HTMLElement
+
+const root = ReactDOMClient.createRoot(container)
+
+root.render(<React.StrictMode>
+	<App />
+</React.StrictMode>)
