1635d45bb4d9b79d2ebaf296aea16a47259f6806
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-05-23 14:11:43 +0100

    fixed age in about me

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -7,18 +7,14 @@ import { AboutParagraph, AboutTitle, ParagraphWrapper, TitleWrapper } from './Ab
 const About: FC = () => {
 
 	const getAge = (): number => {
-		const currentDate = +new Date()
-		const birthDate = 957567600000
-
-		const difference = currentDate - birthDate
-		const yearsDifference = ~~(difference / 1000 / 60 / 60 / 24 / 365) - 1
-
-		return yearsDifference
+		const difference = Date.now() - 957567600000
+		const ageDate = new Date(difference)
+		return Math.abs(ageDate.getUTCFullYear() - 1970)
 	}
 
 	const paragraph = [
-		`Hi, I\`m a ${getAge()} year old self taught programmer from London.`,
-		'Ever since I was little I\'ve had an interest in programming and technology. Over the last six years I\'ve managed to turn that interest into a hobby and passion. Now i\'m looking to that it that next step and turn it into a career.',
+		`Hi, I'm a ${getAge()} year old self taught programmer from London.`,
+		'Ever since I was little I\'ve had an interest in programming and technology. Over the last six years I\'ve managed to turn that interest into a hobby and passion. Now I\'m looking to that it that next step and turn it into a career.',
 		'Currently I\'m an entry level front end developer currently looking for employment. I\'ve been learning and using JavaScript for six years and React for two as well as various other languages and technologies. I\'m seeking a React developer position to purse my passion for web development and creating new experiences.'
 	].join('\n\n')
 
