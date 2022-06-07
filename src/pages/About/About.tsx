dcfba8ff544da10f84f6d526c393c4d3fa346fb2
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-06-07 17:51:37 +0100

    fix typo in about me

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -18,7 +18,7 @@ const About: FC = () => {
 	const paragraph = [
 		`Hi, I'm a ${getAge()} year old self taught programmer from London.`,
 		'Ever since I was little I\'ve had an interest in programming and technology. Over the last six years I\'ve managed to turn that interest into a hobby and passion. Now I\'m looking to that it that next step and turn it into a career.',
-		'Currently I\'m an entry level front end developer currently looking for employment. I\'ve been learning and using JavaScript for six years and React for two as well as various other languages and technologies. I\'m seeking a React developer position to purse my passion for web development and creating new experiences.'
+		'Currently I\'m an entry level front end developer currently looking for employment. I\'ve been learning and using JavaScript for six years and React for two as well as various other languages and technologies. I\'m seeking a React developer position to pursue my passion for web development and creating new experiences.'
 	].join('\n\n')
 
 	return !isMobile ? (
